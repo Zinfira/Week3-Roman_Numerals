b34316173667d4478e3d0ef882526e5dd7c11880
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-02 10:28:27 -0700

    add split function for key, set while loop.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,16 @@ function convertToRoman(number) {
   console.log(number);
   if(!+number)
       return false;
-  
+  var digits = String(+number).split("");
+      key=["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+            "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+            "","I","II","III","IV","V","VI","VII","VIII","IX"];
+      roman="";
+      i = 3;
+      while (i--)
+      
+
+
 }
 
 
@@ -26,7 +35,7 @@ $(document).ready(function() {
 
 
 
-
+  
 
 
 
