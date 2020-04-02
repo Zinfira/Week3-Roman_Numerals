613856d0df91fe830f3d7bc5c4929359dc83d2e5
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-02 09:41:43 -0700

    updat html file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
+var convertToRoman = function(number) {
+  var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 5, 4, 1];
+  var romanNum = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var roman = "";
+
+  for (var index = 0; index < decimalValue.length; index ++) {
+    while (decimal)
+  }
+
+}
+
+
+
+
 // First rule I=1 and II=2.
 $(document).ready(function() {
   $("form#convert").submit(function(event) {
     event.preventDefault();
+    var I = 1;
+    if ("input#input1" === 2)
+    return(II);
   });
 });
 
